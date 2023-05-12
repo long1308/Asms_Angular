71dc8016287088a0d1d689b3b5cb40aaefab56d4
long1308 <lemle2580@gmail.com>
2023-05-12 23:39:47 +0700

    build : Base [UI]-Shared/Page

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/Client/header/header.component';
+import { FooterComponent } from './shared/Client/footer/footer.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { ProductDeailComponent } from './components/product-deail/product-deail.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListProductComponent,
+    ListProductsComponent,
+    ProductDeailComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
