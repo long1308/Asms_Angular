5101b7d3adb8b87d630d4e65f2b1a51070758d66
0o0ok0o0 <longka14pro@gmail.com>
2023-05-15 18:08:18 +0700

    refactor: ProductDetail -style -thêm roterlink: login, sinup, listproducts, trang home, menu detail -sửa màu chữ product detail, styke slider -logic slider

--- Angular/src/app/components/product-detail/carousel/carousel.component.ts
+++ Angular/src/app/components/product-detail/carousel/carousel.component.ts
@@ -7,10 +7,7 @@ import { SlickCarouselComponent } from 'ngx-slick-carousel';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
-  @ViewChild(SlickCarouselComponent, { static: true }) carousel!: SlickCarouselComponent;
-  constructor() {
 
-  }
   onchange(index: number) {
     this.selectedIndex = index;
 
@@ -25,22 +22,15 @@ export class CarouselComponent {
   }
   selectedIndex:number = 0;
   productImages: string[] = [
-    'https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    "https://shopgiayreplica.com/wp-content/uploads/2021/07/top-giay-sneaker-hot-2021-rotated.jpg",
+    'https://toigingiuvedep.vn/wp-content/uploads/2021/06/hinh-anh-nen-dong-anime.gif',
     'https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
     'https://www.hdnicewallpapers.com/Walls/Big/Cat/Beautiful_Cat_Imge_Download.jpg',
     'https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    'https://www.hdnicewallpapers.com/Walls/Big/Cat/Beautiful_Cat_Imge_Download.jpg',
-    'https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    "https://cdn.alongwalk.info/vn/wp-content/uploads/2022/09/28150733/image-69-hinh-anh-gai-xinh-trung-quoc-hot-girl-trung-quoc-dep-nhat-2022-166432725338362.jpg"
+    
+ 
+    
   ];
 
-  sliderConfig = {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000
-  };
+  
 }
