a2535f68cf7a7b0799ac82b208f6223d7e869ca1
long1308 <lemle2580@gmail.com>
2023-05-17 08:12:23 +0700

    fix: [BE]- Color/Size-<toLowerCase>

--- Nodejs/src/controllers/size.js
+++ Nodejs/src/controllers/size.js
@@ -47,7 +47,9 @@ export const createSize = async (req, res) => {
     }
     //check name
     const sizeAll = await Size.find();
-    const sizeName = sizeAll.find((size) => size.name === req.body.name);
+    const sizeName = sizeAll.find(
+      (size) => size.name.toLowerCase() === req.body.name.toLowerCase()
+    );
     if (sizeName) {
       return res.status(400).json({
         message: "Tên size đã tồn tại !",
@@ -76,6 +78,16 @@ export const updateSize = async (req, res) => {
     if (error) {
       return res.status(400).json({
         message: error.details.map((error) => error.message),
+      });
+    }
+    //check name
+    const sizeAll = await Size.find();
+    const sizeName = sizeAll.find(
+      (size) => size.name.toLowerCase() === req.body.name.toLowerCase()
+    );
+    if (sizeName) {
+      return res.status(400).json({
+        message: "Tên size đã có trong danh sách hoặc đã tồn tại !!",
       });
     }
     const size = await Size.findByIdAndUpdate({ id: req.params.id }, req.body, {
