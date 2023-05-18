33925867102a51364de874bd5c0050eeb3dda4e6
long1308 <lemle2580@gmail.com>
2023-05-19 01:05:18 +0700

    refactor: [BE] product model

--- Nodejs/src/models/product.js
+++ Nodejs/src/models/product.js
@@ -6,7 +6,9 @@ const productSchema = new Schema(
     name: String,
     price: Number,
     priceSale: Number,
-    image: String,
+    image: Array,
+    rating: Number,
+    quantity: Number,
     description: String,
     description_short: String,
     featured: {
@@ -25,12 +27,26 @@ const productSchema = new Schema(
       type: [String],
       enum: ["green", "blue", "pink", "red", "indigo"],
     },
+    inventoryStatus: {
+      type: String,
+      enum: ["INSTOCK", "LOWSTOCK", "OUTOFSTOCK"],
+    },
   },
   { timestamps: true, versionKey: false }
 );
 productSchema.pre("save", function (next) {
   if (this.isModified("hot_sale") || this.isModified("price")) {
     this.priceSale = this.price * (1 - this.hot_sale / 100);
+  }
+  switch (true) {
+    case this.quantity <= 0:
+      this.inventoryStatus = "OUTOFSTOCK";
+      break;
+    case this.quantity <= 10:
+      this.inventoryStatus = "LOWSTOCK";
+      break;
+    default:
+      this.inventoryStatus = "INSTOCK";
   }
   next();
 });
