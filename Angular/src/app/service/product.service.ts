ed9ea329a07541a044c002efbae4cc858ab36104
long1308 <lemle2580@gmail.com>
2023-05-25 00:06:30 +0700

    refactor: [UX] add to cart client

--- Angular/src/app/service/product.service.ts
+++ Angular/src/app/service/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Iproduct } from '../interface/product';
-import { signup , signin } from '../interface/user';
+import { signup, signin } from '../interface/user';
 
 @Injectable({
   providedIn: 'root',
@@ -17,14 +17,20 @@ export class ProductService {
   getProduct(id: number | string): Observable<Iproduct> {
     return this.http.get<Iproduct>('http://localhost:8080/api/products/' + id);
   }
-  register(user: signup) :Observable<signup>{
-    return this.http.post<signup>('http://localhost:8080/api/signup',user);
+  register(user: signup): Observable<signup> {
+    return this.http.post<signup>('http://localhost:8080/api/signup', user);
   }
 
-  login(user: signin) :Observable<signin> {
-    return this.http.post<signin>('http://localhost:8080/api/signin',user);
+  login(user: signin): Observable<signin> {
+    return this.http.post<signin>('http://localhost:8080/api/signin', user);
   }
   editProduct(product: Iproduct): Observable<Iproduct> {
-    return this.http.put<Iproduct>('http://localhost:8080/api/products/' + product._id, product);
+    return this.http.put<Iproduct>(
+      'http://localhost:8080/api/products/' + product._id,
+      product
+    );
+  }
+  addCart(product: any): Observable<any> {
+    return this.http.post<any>('http://localhost:8080/api/cart', product);
   }
 }
